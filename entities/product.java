a5ddeb70457901e79be25dabdea2eaa366d7ecff
edo41a <brownemgd@gmail.com>
2023-09-29 22:11:41 -0500

    Description attribute for product is gone for sql and java

--- entities/product.java
+++ entities/product.java
@@ -7,7 +7,6 @@ import java.sql.PreparedStatement;
 
 public class product {
     String Name = null;
-    String Description = null;
     BigDecimal Price = new BigDecimal(0);
     String Category = null;
 
@@ -16,21 +15,19 @@ public class product {
         this.Category = Category;
     }
 
-    public product(String Name, String Category, String Description, Double Price) {
+    public product(String Name, String Category, Double Price) {
         this.Name = Name;
         this.Category = Category;
-        this.Description = Description;
         this.Price = new BigDecimal(Price).setScale(2, RoundingMode.HALF_UP);
     }
 
     public void createProduct(Connection conn) {
         try {
-            String sql = "INSERT INTO product (name, category, price, description) VALUES (?, ?, ?, ?)";
+            String sql = "INSERT INTO product (name, category, price) VALUES (?, ?, ?)";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, Name);
             pstmt.setString(2, Category);
             pstmt.setBigDecimal(3, Price);
-            pstmt.setString(4, Description);
             int affectedRows = pstmt.executeUpdate();
             if (affectedRows > 0) {
                 System.out.println("product added successfully!");
