065391fe9b0155bf3ba3092fe10100d90002d64d
Enigmap <samtrythall@gmail.com>
2023-10-08 16:42:43 -0500

    All button implementation for OrderPage is done except for proceed, all thats left is to integrate the backend with the first two pages for full functionality.

--- src/pointOfSales/menuItemButtonController.java
+++ src/pointOfSales/menuItemButtonController.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 
 public class menuItemButtonController {
@@ -17,14 +18,19 @@ public class menuItemButtonController {
 
     @FXML
     private void handleSubButton(ActionEvent event){
+        AnchorPane subPane = orderPage.getSubMenu();
+        subPane.setVisible(!subPane.isVisible());
+        AnchorPane mainPane = orderPage.getMainMenu();
+        mainPane.setVisible(!mainPane.isVisible());
+        orderPage.setFoodCost(5.00);
+        orderPage.getFoodLabel().setText("Food Item $5.00 ");
+    }
+
+    @FXML
+    private void addToTable(ActionEvent event){
         ObservableList<Object[]> data = FXCollections.observableArrayList(
             new Object[]{"MilkTea", "1", "$5.00"}, new Object[]{"BrewedTea", "1", "$4.00"}, new Object[]{"FruitTea", "1", "$3.00"}
         );
-
-
-
-        
-
         orderPage.getTable().setItems(data);
     }
 }
