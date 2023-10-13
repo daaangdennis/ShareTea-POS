43a9ddae89920ce6f4a8d6418cc28abe2f7526a8
edo41a <brownemgd@gmail.com>
2023-10-13 10:46:18 -0500

    Added 3 sale report functions and merged addOrderFull into SystemFunctions

--- entities/product.java
+++ entities/product.java
@@ -1,5 +1,4 @@
 package entities;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.sql.Connection;
--- services/SystemFunctions.java
+++ services/SystemFunctions.java
@@ -4,6 +4,34 @@ import entities.*;
 import java.util.ArrayList;
 
 public class SystemFunctions {
+
+
+    // ------------------ ORDER FUNCTIONS ----------------------// 
+    
+    public static String nextOrderID(){
+        dbconnect dbconn = new dbconnect();
+        Connection conn = dbconn.conn;
+        return (order.nextAvailableOrder(conn)+"");
+    }
+
+    public static void addOrder(String customerFirst, String customerLast, String employeeFirst, String employeeLast, ArrayList<orderProduct> orderProducts, double orderTotal) {
+        dbconnect dbconn = new dbconnect();
+        Connection conn = dbconn.conn;
+
+        int customerID = customer.getCustomerByName(conn, customerFirst, customerLast);
+        int employeeID = employee.getEmployeeByName(conn, employeeFirst, employeeLast);
+        int order_id = order.createOrder(conn, customerID, employeeID, orderTotal);
+
+        for(int i = 0; i < orderProducts.size(); ++i){
+            int productID = orderProducts.get(i).ProductID;
+            orderProducts.get(i).addOrderProduct(conn, productID, order_id);
+        }
+    }
+
+
+
+    // ------------------ SYSTEM FUNCTIONS ----------------------// 
+
     public static ArrayList<String> verify(String PW){
         dbconnect dbconn = new dbconnect();
         Connection conn = dbconn.conn;
@@ -21,8 +49,6 @@ public class SystemFunctions {
         Connection conn = dbconn.conn;
         return product.getProductsPriceByCategory(conn, Category);
     }
-
-
 
     public static ArrayList<String> getCategories(){
         dbconnect dbconn = new dbconnect();
@@ -58,6 +84,28 @@ public class SystemFunctions {
         dbconnect dbconn = new dbconnect();
         Connection conn = dbconn.conn;
         product.deletePro(conn, productName);
+    }
+
+
+
+    // ------------------ SALE REPORTS FUNCTIONS ----------------------// 
+
+    public static ArrayList<ArrayList<Object>> getProductSales(String startDate, String endDate){
+        dbconnect dbconn = new dbconnect();
+        Connection conn = dbconn.conn;
+        return sales.ProductSales(conn, startDate,endDate);
+    }
+
+    public static ArrayList<ArrayList<Object>> getLowStock(){
+        dbconnect dbconn = new dbconnect();
+        Connection conn = dbconn.conn;
+        return sales.lowStock(conn);
+    }
+
+    public static ArrayList<ArrayList<Object>> getExcessStock(String startDate){
+        dbconnect dbconn = new dbconnect();
+        Connection conn = dbconn.conn;
+        return sales.excessStock(conn, startDate);
     }
 
 }
