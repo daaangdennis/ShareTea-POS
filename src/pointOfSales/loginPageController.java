6ccd55abe9ca45bfc8e739a082f7c8a5973d6f9f
Enigmap <samtrythall@gmail.com>
2023-10-13 09:35:48 -0500

    Merging Dennis Branch to my Branch.

--- src/pointOfSales/loginPageController.java
+++ src/pointOfSales/loginPageController.java
@@ -28,67 +28,19 @@ public class loginPageController {
         employeeFirstName = name;
     }
 
-    /**
-     * 
-     * public static void setLastName(String name) {
-     * employeeLastName = name;
-     * }
-     * 
-     * public static void setPosition(String pos) {
-     * employeePosition = pos;
-     * }
-     * /**
-     * 
-     * public static String getFirstName() {
-     * return employeeFirstName;
-     * }
-     * 
-     * public static String getLastName() {
-     * return employeeLastName;
-     * }
-     * 
-     * public static String getPosition() {
-     * return employeePosition;
-     * }
-     * 
-     * @FXML
-     * 
-     *       private void handleNumberButtonClick(ActionEvent event) {
-     *       Button clickedButton = (Button) event.getSource();
-     *       String buttonName = clickedButton.getId();
-     *       char value = buttonName.charAt(buttonName.length() - 1);
-     *       passwordField.appendText(String.valueOf(value));
-     *       }
-     *       /**
-     * 
-     *       This is a handler for the delete
-     *       eg reRemoves a chagracter offrom the passwordfield
-     *       buffer.apeventreference to thereference to the clicking of the button,
-     *       can be used to grab information relating to the eventt no nottu
-     *
-     * 
-     * 
-     * @param name the first name of the employee passed as a String
-     * 
-     * @see String
-     * 
-     */
+    public static void setLastName(String lastname) {
+        employeeLastName = lastname;
+    }
+
+    public static void setPosition(String pos) {
+        employeePosition = pos;
+    }
+
     @FXML
     private void handleDelButtonClick(ActionEvent event) {
         passwordField.deleteText(passwordField.getLength() - 1, passwordField.getLength());
     }
 
-    /**
-     * 
-     * This handler clear the password
-     * 
-     * 
-     * 
-     * @param event onClick event
-     * 
-     * @see ActionEvent
-     * 
-     */
     @FXML
     private void handleClearButtonClick(ActionEvent event) {
         passwordField.clear();
