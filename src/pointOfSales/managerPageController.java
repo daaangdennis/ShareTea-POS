e9cd75a2edb91bd30e20f692305194fd9fd74b75
Enigmap <samtrythall@gmail.com>
2023-10-15 17:56:08 -0500

    preparing to merge with frontend-Dennis

--- src/pointOfSales/managerPageController.java
+++ src/pointOfSales/managerPageController.java
@@ -72,16 +72,16 @@ public class managerPageController implements Initializable {
     @FXML
     private TableColumn<Object[], String> menuPriceColumn;
 
-    @FXML
-    private TableColumn<Object[], String> excessReportTable;
-    @FXML
-    private TableColumn<Object[], String> excessIDColumn;
-    @FXML
-    private TableColumn<Object[], String> excessNameColumn;
-    @FXML
-    private TableColumn<Object[], String> excessStockUsedColumn;
-    @FXML
-    private TableColumn<Object[], String> excessCurrentStockColumn;
+    // @FXML
+    // private TableColumn<Object[], String> excessReportTable;
+    // @FXML
+    // private TableColumn<Object[], String> excessIDColumn;
+    // @FXML
+    // private TableColumn<Object[], String> excessNameColumn;
+    // @FXML
+    // private TableColumn<Object[], String> excessStockUsedColumn;
+    // @FXML
+    // private TableColumn<Object[], String> excessCurrentStockColumn;
 
     @FXML
     private AnchorPane menuItems;
@@ -896,48 +896,51 @@ public class managerPageController implements Initializable {
 
     @FXML
     private void handleExcessReport() {
-        excessData.clear();
-        String excessTime = excessTimestamp.getValue().toString();
-        ArrayList<ArrayList<Object>> excessStock = new ArrayList<>();
-        excessStock = SystemFunctions.getExcessStock(excessTime);
-        for (int i = 0; i < excessStock.get(0).size(); i++) {
-            excessData.add(new Object[] { excessStock.get(0) });
-        }
-        inventoryTable.setItems(inventoryData);
+        // excessData.clear();
+        // String excessTime = excessTimestamp.getValue().toString();
+        // ArrayList<ArrayList<Object>> excessStock = new ArrayList<>();
+        // excessStock = SystemFunctions.getExcessStock(excessTime);
+        // for (int i = 0; i < excessStock.get(0).size(); i++) {
+        //     excessData.add(new Object[] { excessStock.get(0) });
+        // }
+        // inventoryTable.setItems(inventoryData);
     }
 
     private void initializeExcessTable() {
-        excessIDColumn.setCellValueFactory(cellData -> {
-            if (cellData.getValue() != null && cellData.getValue().length > 0) {
-                return new SimpleStringProperty(cellData.getValue()[0].toString());
-            } else {
-                return new SimpleStringProperty("Inventory ID");
-            }
-        });
+        // excessIDColumn.setCellValueFactory(cellData -> {
+        // if (cellData.getValue() != null && cellData.getValue().length > 0) {
+        // return new SimpleStringProperty(cellData.getValue()[0].toString());
+        // } else {
+        // return new SimpleStringProperty("Inventory ID");
+        // }
+        // });
 
-        excessNameColumn.setCellValueFactory(cellData -> {
-            if (cellData.getValue() != null && cellData.getValue().length > 1) { // Use index 1 for quantity
-                return new SimpleStringProperty(cellData.getValue()[1].toString());
-            } else {
-                return new SimpleStringProperty("Item Name");
-            }
-        });
+        // excessNameColumn.setCellValueFactory(cellData -> {
+        // if (cellData.getValue() != null && cellData.getValue().length > 1) { // Use
+        // index 1 for quantity
+        // return new SimpleStringProperty(cellData.getValue()[1].toString());
+        // } else {
+        // return new SimpleStringProperty("Item Name");
+        // }
+        // });
 
-        excessStockUsedColumn.setCellValueFactory(cellData -> {
-            if (cellData.getValue() != null && cellData.getValue().length > 2) { // Use index 2 for price
-                return new SimpleStringProperty(cellData.getValue()[2].toString());
-            } else {
-                return new SimpleStringProperty("Stock Used");
-            }
-        });
+        // excessStockUsedColumn.setCellValueFactory(cellData -> {
+        // if (cellData.getValue() != null && cellData.getValue().length > 2) { // Use
+        // index 2 for price
+        // return new SimpleStringProperty(cellData.getValue()[2].toString());
+        // } else {
+        // return new SimpleStringProperty("Stock Used");
+        // }
+        // });
 
-        excessCurrentStockColumn.setCellValueFactory(cellData -> {
-            if (cellData.getValue() != null && cellData.getValue().length > 3) { // Use index 2 for price
-                return new SimpleStringProperty(cellData.getValue()[3].toString());
-            } else {
-                return new SimpleStringProperty("Current Stock");
-            }
-        });
+        // excessCurrentStockColumn.setCellValueFactory(cellData -> {
+        // if (cellData.getValue() != null && cellData.getValue().length > 3) { // Use
+        // index 2 for price
+        // return new SimpleStringProperty(cellData.getValue()[3].toString());
+        // } else {
+        // return new SimpleStringProperty("Current Stock");
+        // }
+        // });
     }
 
 }
