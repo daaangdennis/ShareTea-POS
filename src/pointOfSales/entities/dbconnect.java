981f615c0885745b8c795f1d00d0b95f1afb2ccb
ahbalbaid <ahbalbaid@tamu.edu>
2023-10-16 20:29:47 -0500

    javadoc dbconnect

--- src/pointOfSales/entities/dbconnect.java
+++ src/pointOfSales/entities/dbconnect.java
@@ -3,9 +3,19 @@ package pointOfSales.entities;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+ * Handles the connection to the database for the point of sales system.
+ */
 public class dbconnect {
+    /** The database connection. */
     public Connection conn;
 
+    /**
+     * Constructs a dbconnect object and initializes a connection to the database.
+     * It uses configurations from the dbconfig class for the username and password.
+     * If the connection is successful, a message is printed to the console.
+     * If there's an error, the error details are printed to the error console.
+     */
     public dbconnect() {
         try {
             Class.forName("org.postgresql.Driver");
